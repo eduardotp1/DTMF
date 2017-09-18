4be9e6fc95946d7977914dba2da84d880caef8b0
Gabriel Moreira <gmore@Gabriels-MacBook-Pro.local>
2017-09-18 14:35:31 -0300

    plot

--- decoderDTMF.py
+++ decoderDTMF.py
@@ -1,4 +1,16 @@
 import sounddevice as sd
-#2
+import matplotlib.pylab as plt
+import numpy as np
+
+
 def audioListener():
     
+    #Plotting
+    x = np.linspace(-np.pi, np.pi, 201)
+    plt.plot(x, np.sin(x))
+    plt.xlabel('Angle [rad]')
+    plt.ylabel('sin(x)')
+    plt.axis('tight')
+    plt.show()
+
+audioListener()
--- encoderDTMF.py
+++ encoderDTMF.py
@@ -5,6 +5,17 @@ def audioGenetator():
     
     audio = sd.rec(int(1*fs), fs, channels=1)
     sd.wait()
+    
+    num_1 = [697,1209]
+    num_2 = [697,1336]
+    num_3 = [697,1477]
+    num_4 = [770,1209]
+    num_5 = [770,1336]
+    num_6 = [770,1477]
+    num_7 = [852,1209]
+    num_8 = [852,1336]
+    num_9 = [852,1477]
+    num_0 = [941,1336]
 
     y = audio[:,0]
 
