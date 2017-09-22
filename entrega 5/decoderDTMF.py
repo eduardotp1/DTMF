b060440e5601c460d6b9e166af188975d3d69891
Gabriel Moreira <gmore@Gabriels-MacBook-Pro.local>
2017-09-22 14:57:08 -0300

    tones2

--- entrega 5/decoderDTMF.py
+++ entrega 5/decoderDTMF.py
@@ -5,30 +5,45 @@ import sched, time
 import threading
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import cPickle as pickle
+from scipy.fftpack import fft
+from scipy import signal as window
+import soundfile as sf
 
 
-fs = 44100
-
+fs = 44100.0
+T  = 1.0/fs
+N = 1000
 #Plotting
 duration = 1
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-plt.xlabel('Time')
-plt.ylabel('sin(x)')
+f, ax1 = plt.subplots(2, sharex=False)
+
+def calcFFT(signal, fs):
+	from scipy.fftpack import fft
+	from scipy import signal as window
+
+	N  = len(signal)
+	T  = 1/fs
+	xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+	yf = fft(signal)
+	return(xf, np.abs(yf[0:N//2]))
+
 
 def animate(i):
-    audio = sd.rec(int(1*fs), fs, channels=1)
-    y = audio[:,0]
-    t = np.linspace(0,1,fs*duration)    
-    ax1.clear()
-    ax1.plot(t[0:1000],y[0:1000])
-    with open('tone0.txt', 'w+') as f:
-        pickle.dump(y, f)
+	fs = 44100.0
+	audio = sd.rec(int(duration*fs), fs, channels=1)
+	y = audio[:,0]
+	t = np.linspace(0,1,fs*duration)
+	X, Y = calcFFT(y,fs)
+	ax1[0].clear()
+ 	ax1[0].plot(t[0:1000],y[0:1000])
+	ax1[1].clear()
+ 	ax1[1].plot(X,Y)
 
     
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+    
+ani = animation.FuncAnimation(f, animate, interval=1000)
+
 plt.show()
 
 
